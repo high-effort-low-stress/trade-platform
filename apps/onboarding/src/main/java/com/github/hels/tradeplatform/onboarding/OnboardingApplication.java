d40612e721ad52531eb4b7f2f0bd3d56bc76cbf0
Caio Torres <caiocftorres@gmail.com>
2023-08-20 14:35:08 -0300

    build: change parent to dependency management

--- apps/onboarding/src/main/java/com/github/hels/tradeplatform/onboarding/OnboardingApplication.java
+++ apps/onboarding/src/main/java/com/github/hels/tradeplatform/onboarding/OnboardingApplication.java
@@ -1,4 +1,4 @@
-package com.github.hels.assetplatform.onboarding;
+package com.github.hels.tradeplatform.onboarding;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
