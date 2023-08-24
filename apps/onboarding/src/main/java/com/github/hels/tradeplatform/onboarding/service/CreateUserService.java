6b19e0d6b5324191bb6ddcb5645f3dfb0bdb7369
Caio Torres <caiocftorres@gmail.com>
2023-08-24 13:47:40 -0300

    test: creating test cases

--- apps/onboarding/src/main/java/com/github/hels/tradeplatform/onboarding/service/CreateUserService.java
+++ apps/onboarding/src/main/java/com/github/hels/tradeplatform/onboarding/service/CreateUserService.java
@@ -3,18 +3,23 @@ package com.github.hels.tradeplatform.onboarding.service;
 import com.github.hels.tradeplatform.onboarding.exceptions.ApiException;
 import com.github.hels.tradeplatform.onboarding.models.User;
 import com.github.hels.tradeplatform.onboarding.repository.IUserRepository;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 
+import java.time.Clock;
 import java.time.LocalDate;
 import java.time.Period;
 import java.util.List;
+import java.util.Objects;
 
 @Service
+@AllArgsConstructor
 @RequiredArgsConstructor
 public class CreateUserService {
     private final IUserRepository repository;
+    private Clock clock = Clock.systemDefaultZone();
 
     public User execute(String name, String document, String email, String password, String phoneNumber, LocalDate birthDate) {
 
@@ -40,10 +45,13 @@ public class CreateUserService {
             throw new ApiException("User already registered.");
     }
 
-    private void isLegalAge (LocalDate dateOfBirth) {
-        LocalDate currentDate = LocalDate.now();
+    private void isLegalAge(LocalDate birthDate) {
+        LocalDate currentDate = LocalDate.now(clock);
 
-        if(Period.between(dateOfBirth, currentDate).getYears() < 18)
+        if (Objects.isNull(birthDate))
+            throw new RuntimeException("Birth date can't be null");
+
+        if(Period.between(birthDate, currentDate).getYears() < 18)
             throw new ApiException("User must be 18+ years old");
     }
 }
